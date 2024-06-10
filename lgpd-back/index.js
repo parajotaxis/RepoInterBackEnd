74458487d1135c88d528d0adf6032ee51713a308
Jefferson Xavier <parajotaxis@gmail.com>
2024-06-10 13:02:23 -0300

    upd router e index js

--- lgpd-back/index.js
+++ lgpd-back/index.js
@@ -2,14 +2,28 @@ import { express } from "express";
 import { pkg } from "body-parser";
 import { router } from "./routes/router.js";
 
+import sequelize from "./utils/database.js";
+import association from "./models/Association.js"
+
 const app = express();
 const { json, urlencoded}  = pkg;
 
 app.use(json());
 app.use(urlenconded({ extended: true}));
 
-app.listen(3000, function(){
-    console.log("Listening from 3000");
-});
+(async () => {
+    try {
+        association.associations();
+        await sequelize.sycn();
+        app.listen(3000, function(){
+            console.log("Listening from 3000");
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+
 
 app.use("/", router);
---
+++ lgpd-back/routes/router.js
@@ -0,0 +1,19 @@
+import express from "express";
+let router = express.Router();
+
+import userController from "./UserController";
+import courseController from "./CourseController";
+import teacherController from "./TeacherController";
+import evaluationController from "./EvalauationController";
+
+router.get("/", function(req, res){
+    console.log("oi!");
+    res.status(200).json({message: "oi!"});
+});
+
+router.use("/", userController);
+router.use("/", courseController);
+router.use("/", teacherController);
+router.use("/", evalutationController);
+
+export default router;
