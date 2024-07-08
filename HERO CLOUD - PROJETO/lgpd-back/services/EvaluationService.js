85e13735b293ea1da901638005a3c7a37f2eb66c
Jefferson Xavier <parajotaxis@gmail.com>
2024-07-07 21:18:12 -0300

    ups services atemp 1

--- HERO CLOUD - PROJETO/lgpd-back/services/EvaluationService.js
+++ HERO CLOUD - PROJETO/lgpd-back/services/EvaluationService.js
@@ -1,7 +1,31 @@
 import evalutationRepository from "../repositories/EvaluationRepository.js"
 
-const service = {
+const saveEvaluation = (evaluationModel) => {
+    return evaluationRepository.saveEvaluation(evaluationModel);
+}
 
+const getEvaluationById = (id) => {
+    return evaluationRepository.getEvaluationById(id);
+}
+
+const getAllEvaluations = () => {
+    return evaluationRepository.getAllEvaluations();
+}
+
+const deleteEvaluationById = (id) => {
+    return evaluationRepository.deleteEvaluationById(id);
+}
+
+const updateEvaluationById = (id, evaluationModel) => {
+    return evaluationRepository.updateEvaluationById(id, evaluationModel);
+}
+
+const service = {
+    saveEvaluation,
+    getEvaluationById,
+    getAllEvaluations,
+    deleteEvaluationById,
+    updateEvaluationById
 }
 
 export default service;
