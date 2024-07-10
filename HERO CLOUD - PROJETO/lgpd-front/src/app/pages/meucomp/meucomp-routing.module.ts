de1bae62c6de999da902027123c563607fc9d9ca
Jefferson Xavier <67285300+parajotaxis@users.noreply.github.com>
2024-07-10 15:36:22 -0300

    upd routing module ts for all

--- HERO CLOUD - PROJETO/lgpd-front/src/app/pages/meucomp/meucomp-routing.module.ts
+++ HERO CLOUD - PROJETO/lgpd-front/src/app/pages/meucomp/meucomp-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {MeucompFormComponent} from "./meucomp-form/meucomp-form.component";
+import {MeucompListComponent} from "./meucomp-list/meucomp-list.component";
+
+const routes: Routes = [
+  {
+    path: 'meucomps',
+    component: MeucompListComponent
+  },
+  {
+    path: 'addMeucomp',
+    component: MeucompFormComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
