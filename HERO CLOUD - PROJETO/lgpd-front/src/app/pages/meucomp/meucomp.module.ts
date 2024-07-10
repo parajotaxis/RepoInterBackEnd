39fe4b2692e59d08b9a2e88275a1408eda22a45e
Jefferson Xavier <67285300+parajotaxis@users.noreply.github.com>
2024-07-10 16:57:34 -0300

    upd modules ts

--- HERO CLOUD - PROJETO/lgpd-front/src/app/pages/meucomp/meucomp.module.ts
+++ HERO CLOUD - PROJETO/lgpd-front/src/app/pages/meucomp/meucomp.module.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { MeucompRoutingModule } from './meucomp-routing.module';
 
+//alterações add esses imports
+import {SharedModule} from "../../shared/shared.module";
+import {MeucompFormComponent} from "./meucomp-form/meucomp-form.component";
+import {MeucompListComponent} from "./meucomp-list/meucomp-list.component";
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MeucompListComponent,
+    MeucompFormComponent
+  ],
   imports: [
     CommonModule,
-    MeucompRoutingModule
+    MeucompRoutingModule,
+    SharedModule
   ]
 })
 export class MeucompModule { }
--- HERO CLOUD - PROJETO/lgpd-front/src/app/pages/user/user.module.ts
+++ HERO CLOUD - PROJETO/lgpd-front/src/app/pages/user/user.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import {UserRoutingModule} from "./user-routing.module";
+
 import {SharedModule} from "../../shared/shared.module";
 import { UserFormComponent } from './user-form/user-form.component';
 import { UserListComponent } from './user-list/user-list.component';
